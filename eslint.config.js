26a2eae90fa624b370d36722175216000db65550
Eric Monterozo <ric.monterozo@gmail.com>
2025-05-30 15:57:56 +0800

    chore: initial commit & config setup

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+
+module.exports = defineConfig([
+  expoConfig,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ["dist/*"],
+    plugins: {
+      "react-native": require("eslint-plugin-react-native"),
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      "no-console": "error",
+      "@typescript-eslint/no-shadow": "error",
+      "react-native/no-unused-styles": "error",
+      "react-native/no-inline-styles": "error",
+    },
+  },
+]);
